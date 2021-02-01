82cca4ac50acf6e9b52320d0559b0fe93bc6a937
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-02-01 19:27:22 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="button" to="/">Home</router-link>
+      <router-link class="button" v-if="this.$store.state.user" to="/profile"
+        >Profile</router-link
+      >
+      <router-link class="button" v-if="!this.$store.state.user" to="/login">
+        Login</router-link
+      >
+      <button v-else type="button" class="button" @click="logout">
+        Logout
+      </button>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -28,5 +45,29 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#nav > button {
+  padding: 0.6rem 1rem;
+  cursor: pointer;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 50px;
+  width: 5rem;
+  outline: none;
+  transition: 0.3s;
+  margin: 0 auto;
+  font-size: 14px;
+  font-weight: bold;
+}
+
+#nav > button:hover {
+  background-color: #42b983;
+  color: white;
+}
+
+.button {
+  padding: 5px;
+  text-decoration: none;
 }
 </style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="profile">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Welcome {{ email }}</h1>
+    <h3>
+      Your Public Address is: <i>{{ publicAddress }}</i>
+    </h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: this.$store.state.user.email,
+      issuer: this.$store.state.user.issuer,
+      publicAddress: this.$store.state.user.publicAddress
+    }
+  }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 20px 0 0;
+}
+</style>
